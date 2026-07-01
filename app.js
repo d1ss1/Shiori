f9d6bafaeb23bf83fe1f7f9b02cc57c47ceea4d2
d1ss1 <hfyd4006@gmail.com>
2026-07-01 22:21:20 +0300

    feat: search + API + render cards

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-fetch('https://api.jikan.moe/v4/anime?q=naruto')
-.then(response => response.json())
-.then(data => console.log(data))
+const searchInput = document.getElementById("searchInput");
+const animeGrid = document.querySelector(".animeGrid");
+
+function renderData(animeList) {
+  animeGrid.innerHTML = "";
+  animeList.forEach(element => {
+    const divCard = document.createElement("div");
+    divCard.classList.add("card");
+    divCard.textContent = element.title;
+    animeGrid.appendChild(divCard)
+  });
+} 
+searchInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    const text = searchInput.value.trim();
+    fetch(`https://api.jikan.moe/v4/anime?q=${text}`)
+    .then(response => response.json())
+    .then(data => renderData(data.data))
+  }
+});
