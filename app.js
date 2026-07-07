cf277339db4fa4b39c28e27f228dfa868d64a96a
d1ss1 <hfyd4006@gmail.com>
2026-07-07 20:49:33 +0300

    update: error handling, top anime

--- app.js
+++ app.js
@@ -3,23 +3,48 @@ const animeGrid = document.querySelector(".animeGrid");
 
 function renderData(animeList) {
   animeGrid.innerHTML = "";
-  animeList.forEach(element => {
+  animeList.forEach((element) => {
     const divCard = document.createElement("div");
     const spanCard = document.createElement("span");
     const posterImg = document.createElement("img");
-    posterImg.src = element.images.jpg.image_url
+    posterImg.src = element.images.jpg.image_url;
     divCard.classList.add("card");
     divCard.appendChild(posterImg);
     spanCard.textContent = element.title;
     divCard.appendChild(spanCard);
     animeGrid.appendChild(divCard);
   });
-} 
+}
 searchInput.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     const text = searchInput.value.trim();
     fetch(`https://api.jikan.moe/v4/anime?q=${text}`)
-    .then(response => response.json())
-    .then(data => renderData(data.data))
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Server error, status: ${response.status}`);
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        renderData(data.data);
+      })
+      .catch((error) => {
+        console.error("Error during request:", error.message);
+      });
   }
 });
+fetch("https://api.jikan.moe/v4/top/anime")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Server error, status: ${response.status}`);
+    } else {
+      return response.json();
+    }
+  })
+  .then((data) => {
+    renderData(data.data);
+  })
+  .catch((error) => {
+    console.error("Error during request:", error.message);
+  });
